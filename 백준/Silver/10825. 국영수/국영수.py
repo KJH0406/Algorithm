f62b753f32cd9d51c160c170394620a08c523f05
KJH0406 <109582129+KJH0406@users.noreply.github.com>
2023-02-14 01:05:39 +0900

    [Silver IV] Title: 국영수, Time: 460 ms, Memory: 87636 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,9 +1,12 @@
 import sys
+from operator import itemgetter
 
-N = int(sys.stdin.readline().rstrip())
-students = [sys.stdin.readline().rstrip().split() for _ in range(N)]
+students = []
+N = int(input())
+for i in range(N):
+    students.append(sys.stdin.readline().split())
 
-students.sort(key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+students = sorted(students, key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
 for name in students:
     print(name[0])
