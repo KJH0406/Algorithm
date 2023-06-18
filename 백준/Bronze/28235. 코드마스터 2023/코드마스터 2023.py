efd49865e30f8bdea3889ba4fc5dec08b9f06630
KJH0406 <109582129+KJH0406@users.noreply.github.com>
2023-06-18 18:29:29 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,9 @@
+char = input()
+if char == 'SONGDO':
+    print('HIGHSCHOOL')
+elif char == 'CODE':
+    print('MASTER')
+elif char == '2023':
+    print('0611')
+elif char == 'ALGORITHM':
+    print('CONTEST')
