7072f1dbfa5ac5b519c19ab9b7530c48c1de88d5
KJH0406 <109582129+KJH0406@users.noreply.github.com>
2023-06-23 14:50:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 35400 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,15 +1,12 @@
 from collections import deque
 
 N, K = map(int, input().split())
-point = list(range(100001))
 visited = [0] * 100001
-
 queue = deque()
 queue.append(N)
 visited[N] = 1
 
 flag = True
-res = 0
 
 while flag:
     cp = queue.popleft()
