81faa65ca81493809be0ab293113107c9b76f2ba
djangho <109582129+KJH0406@users.noreply.github.com>
2024-01-10 18:24:39 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1196 ms, Memory: 158856 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -2,38 +2,24 @@ from collections import deque
 
 N, M, K, X = map(int, input().split())
 
-roads = {}
+roads = [[] for _ in range(N + 1)]
 
 for _ in range(M):
     A, B = map(int, input().split())
-    if A in roads:
-        roads[A].append(B)
-    else:
-        roads[A] = [B]
+    roads[A].append(B)
 
-distance = [0] * (N + 1)
-
-queue = deque()
-queue.append(X)
-distance[X] = 1
-
-start = True
+distance = [-1] * (N + 1)
+distance[X] = 0
+queue = deque([X])
 
 while queue:
-    if start == True:
-        distance[X] = 0
-        start = False
-    else:
-        distance[X] = 1
     current = queue.popleft()
-    if current in roads:
-        for city in roads[current]:
-            if distance[city] == 0:
-                distance[city] = distance[current] + 1
-                queue.append(city)
+    for city in roads[current]:
+        if distance[city] == -1:
+            distance[city] = distance[current] + 1
+            queue.append(city)
 
 flag = 1
-distance[X] = 0
 
 for i in range(N + 1):
     if distance[i] == K:
