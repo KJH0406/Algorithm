2428f90b06303dbb5a32782f95da3d21a277367f
JangHo Kim <109582129+KJH0406@users.noreply.github.com>
2024-01-12 15:53:37 +0900

    [Gold IV] Title: 공유기 설치, Time: 176 ms, Memory: 112084 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,39 +1,48 @@
 import sys
 input = sys.stdin.readline
 
-n, c = map(int, input().split())  # N: 집의 개수, C : 설치해야 하는 공유기의 개수
-house_list = [int(input()) for _ in range(n)]  # 집 리스트
-house_list.sort()  # 이분탐색을 실행하기 전 반드시 오름차순 정렬
+# n: 집의 개수, c : 설치 해야 하는 공유기의 개수
+n, c = map(int, input().split())
 
+# n개의 집의 좌표 정보 배열
+house_list = [int(input()) for _ in range(n)] 
 
-# gap 차이 이분 탐색
-start = 1
-end = house_list[-1]
-gap = 0
+# 이분 탐색을 실행을 위한 정렬
+# * 이분 탐색을 위한 사전 조건 입니다. 반드시 수행 하셔야 합니다!
+house_list.sort()  
 
-while start <= end:
-    mid = (start + end) // 2  # gap 값
-    house = house_list[0]  # 처음 집과 거리 계산 시작
-    cnt = 1  # 설치된 공유기 개수(배열의 가장 첫 집은 이미 설치)
+# 이분 탐색 실행을 위한 조건 설정
+min_gap = 1  # 최소 거리 차이
+max_gap = house_list[-1] - house_list[0]  # 최대 거리 차이
 
-    # 처음 집과 마지막 집을 제외한 나머지 집 탐색
+res = 0  # 결과 값
+
+# 이분 탐색 실행
+while min_gap <= max_gap:
+    current_gap = (min_gap + max_gap) // 2  # 현재 가장 인접 한 두 공유기의 거리
+    installed_house = house_list[0]  # 공유기를 설치한 집
+    count = 1  # 설치된 공유기 개수
+
+    # 배열의 가장 처음 집과 마지막 집을 제외한 나머지 범위 탐색
     for i in range(1, n):
+        # 현재 탐색하고 있는 집이 이전에 설치 된 집과 현재 거리 차이를 더한 것 보다 같거나 크다면 설치 가능
+        if house_list[i] >= installed_house + current_gap:
+            installed_house = house_list[i]  # 가장 최신에 공유기를 설치한 집 교체
+            count += 1  # 설치된 공유기 개수 증가
 
-        # 설치가 가능한 집 = 마지막으로 설치된 집과 gap을 더했을 때 같거나 작다면
-        if house_list[i] >= house + mid:
-            house = house_list[i]  # 마지막으로 설치한 집 교체
-            cnt += 1  # 설치 개수 증가
 
-        # 만약 설치 된 개수가 많거나 같다면 gap을 조금 더 증가 시켜 판단
-    if cnt >= c:
-        start = mid + 1
-        gap = mid  # 일단 결과 저장
+    # 위 과정을 실행 후 현재 거리 차이로 설치할 수 있는 공유기의 개수로 향후 거리 차이 증감 판단
 
-        # 만약 해당 gap차이를 설정했을때 다 설치하지 못했다면 gap을 줄여야함
+    # 만약 설치 된 공유기의 개수(count)가 설치 해야 하는 공유기의 개수(c)보다 많거나 같다면 거리 차이 증가 가능
+    if count >= c:
+        min_gap = current_gap + 1  # 최소 거리 차이를 현재 거리 차이에서 1 증가된 값으로 변경
+        res = current_gap  # 현재 거리 차이 저장
+
+        # 만약 현재 거리 차이에서 공유기를 다 설치하지 못했다면 공유기 간 거리 차이를 줄여야 함
     else:
-        end = mid - 1
+        max_gap = current_gap - 1  # 최대 거리 차이를 현재 거리 차이에서 1 감소된 값으로 변경
 
-print(gap)
+print(res)
 
 
 
