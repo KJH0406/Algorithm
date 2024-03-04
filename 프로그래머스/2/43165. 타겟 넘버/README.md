99559f4c1d057fb68afba549ad40e6f71bece372
Kim Jang Ho <109582129+KJH0406@users.noreply.github.com>
2024-03-04 16:55:38 +0900

    [level 2] Title: 타겟 넘버, Time: 1.59 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.89 ms
+메모리: 10.3 MB, 시간: 1.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 04일 14:01:43
+2024년 03월 04일 16:55:33
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,24 +1,19 @@
+from collections import deque
+
 def solution(numbers, target):
     answer = 0
-    
-    memo = {}  # 메모이제이션을 위한 딕셔너리 (key: (index, current_sum), value: 방법의 수)
+    current_sums = deque([0])  # 초기 합계를 0으로 설정하여 시작
 
-    def dfs(index, current_sum):
-        # 탐색이 끝났을 때, current_sum이 target과 일치하는지 확인
-        if index == len(numbers):
-            return 1 if current_sum == target else 0
-        
-        # 이미 계산된 경우, 저장된 결과 반환
-        if (index, current_sum) in memo:
-            return memo[(index, current_sum)]
-        
-        # 현재 숫자를 더하거나 빼는 경우를 모두 탐색
-        count = dfs(index + 1, current_sum + numbers[index]) + dfs(index + 1, current_sum - numbers[index])
-        
-        # 결과 저장 후 반환
-        memo[(index, current_sum)] = count
-        return count
+		# 기존 숫자 배열 순회
+    for number in numbers:
+        next_sums = deque()  	# 탐색할 덱 생성
+        while current_sums :
+            current_sum = current_sums.popleft()
+            next_sums.append(current_sum + number)  # 양수(+) 값 반영
+            next_sums.append(current_sum - number)  # 음수(+) 값 반영
+				
+        current_sums = next_sums  # 기존 합계에 반영
 
-    answer = dfs(0, 0)
-    
+    answer = current_sums.count(target)  # 타겟 수 카운트
+
     return answer
