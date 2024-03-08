2033c469dd98432b1e745885632b179b43b69abd
Kim Jang Ho <109582129+KJH0406@users.noreply.github.com>
2024-03-08 14:24:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.32 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,37 +1,23 @@
 from collections import deque
 
 def solution(maps):
-    answer = 0
+    n = len(maps)
+    m = len(maps[0])
     
-    n = len(maps)  # 세로 길이
-    m = len(maps[0])  # 가로 길이
-    
-    queue = deque()  
-    
-    visited = [[0] * m for _ in range(n) ]  # 최단 거리 저장 배열
-    
-    si = sj = 0 # 출발 지점
-    
-    visited[si][sj] = 1  # 출발 지점 거리 지정
-    
-    queue.append((si, sj))  # 출발 지점 큐에 추가
+    queue = deque([(0, 0)])  # 시작 위치 초기화와 동시에 큐에 추가
+    visited = [[0] * m for _ in range(n)] # 최단 거리를 기록할 방문 배열 생성
+    visited[0][0] = 1  # 시작 지점 방문 표시
     
     while queue:
-        ci, cj = queue.popleft() # 현재 위치
+        ci, cj = queue.popleft()
         
-        # 델타 탐색
+        if ci == n - 1 and cj == m - 1:  # 목적지 도달 시 종료
+            return visited[ci][cj]
+        
         for di, dj in ((0, -1), (0, 1), (1, 0), (-1, 0)):
             ni, nj = ci + di, cj + dj
-            
-            # 탐색 조건 체크
             if 0 <= ni < n and 0 <= nj < m and maps[ni][nj] == 1 and visited[ni][nj] == 0:
+                visited[ni][nj] = visited[ci][cj] + 1
                 queue.append((ni, nj))
-                visited[ni][nj] = visited[ci][cj] + 1  # 최단 거리 기록
-        
-        # 상대 팀 진영 도달 여부 확인
-        if visited[n - 1][m - 1]:
-            answer = visited[n - 1][m - 1]
-        else:
-            answer = -1
-                    
-    return answer
+                
+    return -1  # 목적지에 도달하지 못한 경우
