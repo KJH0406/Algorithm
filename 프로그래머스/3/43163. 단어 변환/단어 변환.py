887f77e0fc7622e1f84ed529fa8098cd616d8b46
Kim Jang Ho <109582129+KJH0406@users.noreply.github.com>
2024-03-08 16:05:32 +0900

    [level 3] Title: 단어 변환, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -3,34 +3,42 @@ from collections import deque
 def solution(begin, target, words):
     answer = 0
     
-    words = [""] + words
+    word_length = target  # 단어 길이
     
-    visited = [0] * len(words)
+    visited = [0] * (len(words) + 1)  # 변환 횟수 저장할 배열 생성
     
-    queue = deque([(0, begin)])
+    queue = deque([(0, begin)])  # 시작 단어 설정
     
-    visited[0] = 1
+    visited[0] = 1  # 시작 단어 방문 표시
     
     
+    # 만약 타겟으로 하는 단어가 단어 배열에 없다면 0을 반환
     if target not in words:
         return 0
     
     else:
         while queue:
             current_idx, current_word = queue.popleft()
+            
+            # 타겟 단어로 변환이 완료되었을 경우 변환 횟수 반환
             if current_word == target:
                 return visited[current_idx] - 1
             else:
-                for i in range(1, len(words)):
+                
+                # 단어 비교
+                for i in range(len(words)):
                     next_word = words[i]
-                    if visited[i] == 0:
+                    next_idx = i + 1
+                    if visited[next_idx] == 0:
                         diff = 0
                         for j in range(len(target)):
                             if current_word[j] != next_word[j]:
                                 diff += 1
+                            if diff > 1:
+                                break
+                        # 한 번에 한개의 알파벳만 바꿀 수 있음
                         if diff == 1:
-                            queue.append((i, next_word))
-                            visited[i] = visited[current_idx] + 1
+                            queue.append((next_idx, next_word))
+                            visited[next_idx] = visited[current_idx] + 1
 
     
-    # return answer
